4c25b1d0dad7ea0a54227b6fe2ba8d40756771df
RichardHD <56202253+irichardo@users.noreply.github.com>
2024-07-24 11:27:17 -0500

    Create pruebaParaJhon.tsx

---
+++ pruebaParaJhon.tsx
@@ -0,0 +1,9 @@
+---
+title: Test SLUG
+date: '2024-07-24'
+tags: [aprendiendo, nextjs , experiencia, diario]
+cover: 'https://pbs.twimg.com/media/GJ6AZ7LbkAAW8I_.png'
+preview: 'Prueba funcional SLUG'
+---
+
+# esta es una prueba para que se vea la funcionalidad de SLUG
